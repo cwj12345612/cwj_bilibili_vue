e30f5f787461e19b06304278569ca650a583bbb2
蔡文姬 <2735103940@qq.com>
2023-06-11 09:29:21 +0800

    动态获取浏览器窗口大小

---
+++ src/views/index.vue
@@ -0,0 +1,54 @@
+<template>
+ <h1>你好世界{{ aa }}</h1>
+ <h2>tgb v csc</h2>
+</template>
+ 
+<script>
+ 
+import { mapGetters,mapActions,mapMutations,mapState } from 'vuex';
+ 
+export default {
+    components:{
+ 
+              },
+    data() {
+          return {
+            aa:'defrvf v'
+                 }
+         },
+    computed:{
+
+             },
+    created() {
+
+              },
+    mounted() {
+
+              },
+    watch:{
+
+          },
+
+    beforeCreate(){
+
+                  },
+    beforeDestroy() {
+
+                    },
+    deactivated() {
+
+                   },
+}
+</script>
+<style  scoped>
+h1 {
+      width: 50%;
+      height: 50%;
+      background-color: aqua;
+}
+h2 {
+      width: 50%;
+      height: 50%;
+      background-color: gold;
+}
+</style>
