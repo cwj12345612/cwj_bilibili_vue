02dc2db9c463626465fd8af1b3588120081e016b
蔡文姬 <2735103940@qq.com>
2023-08-19 12:59:40 +0800

    ing

--- src/views/home/nav/right.vue
+++ src/views/home/nav/right.vue
@@ -26,27 +26,32 @@ useStore
     //#region 
 // 240x66
 const style=dynamicsize.dynamicWH(
-    {normal:240,max:258,min:168},
+    // {normal:240,max:258,min:240},
+    undefined,
     {normal:66,max:78,min:66},
 )
 
 //#endregion
 const list=reactive([
                 {title:'专栏',herf:'#',iclass:'colourless chuangzuozhongxin'},
+                {title:'社区中心',herf:'#',iclass:'colourless shequ'},
                 {title:'活动',herf:'#',iclass:'colourless huodongxiangqu'},
                 {title:'直播',herf:'#',iclass:'colourless zhiboguankanshu'},
                 {title:'课堂',herf:'#',iclass:'colourless ketang'},
-                {title:'社区中心',herf:'#',iclass:'colourless shequ'},
+              
                 {title:'新歌热榜',herf:'#',iclass:'colourless yinle'},
             ])
 </script>
 <style scoped>
 .right{
+    /* width: 100%; */
     /* background-color: coral; */
 display: grid;
 grid-row-gap: 10px;
+grid-column-gap: 15px;
 grid-auto-flow: column;
 grid-template-rows: repeat(2,1fr);
+grid-template-columns: repeat(3,auto);
 }
 .right li a {
     font-size: 14px;
